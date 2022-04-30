85e785697c62dafb2a25e470310b00441baac262
kms1837 <kms1837@gmail.com>
2022-04-30 20:01:19 +0900

    feat: add scene fade

--- 2nd_Team2/2nd_Team2/GameClient.h
+++ 2nd_Team2/2nd_Team2/GameClient.h
@@ -19,6 +19,7 @@ public:
 
 	void ClearManagers();
 	void LoadScene(CScene*);
+	void SceneSwap();
 
 	void SceneLifeCycle();
 
@@ -26,8 +27,27 @@ private:
 	void Render();
 	void Release();
 
+	void Fading();
+	void FadeEnd();
+	void FadeRender(HDC);
+
 private:
+	const int fadeLimit = 255;
+	const int m_fadeSpeed;
+
+	enum fade {
+		FADE_IN,
+		FADE_OUT,
+		FADE_LENGTH
+	};
+
+	int m_fadeAlpha; // ∫“≈ı∏Ì 255
+	fade m_fadeMode;
+
 	HDC	m_hDC;
 	CScene* m_runningScene;
+	bool m_pause;
+
+	CScene* m_reservationLoadScene;
 };
 
--- 2nd_Team2/2nd_Team2/stdafx.h
+++ 2nd_Team2/2nd_Team2/stdafx.h
@@ -6,6 +6,8 @@
 // Windows 헤더 파일:
 #include <windows.h>
 
+#pragma comment(lib, "msimg32.lib")
+
 // C 런타임 헤더 파일입니다.
 #include <stdlib.h>
 #include <malloc.h>
